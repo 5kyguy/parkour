7e0099fbb07af70a42adb87f67ec7b02638161d9
5kyguy <0x5kyguy@gmail.com>
2026-04-06 15:19:00 +0530

    chore: setup project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './styles/main.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <main>
+    <h1>Parkour</h1>
+    <p>Project scaffold is ready. Start building the movement prototype in <code>src/game</code>.</p>
+  </main>
+`
